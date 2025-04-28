07515eeb977ecfc3548ec20615563e0ecde684cc
DustinHeaps <60202144+DustinHeaps@users.noreply.github.com>
2025-04-27 22:05:22 -0400

    changed to auth

--- app/api/reset-waivers/route.ts
+++ app/api/reset-waivers/route.ts
@@ -5,10 +5,12 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
-  const secret = req.headers.get("cron-secret");
+  // const secret = req.headers.get("cron-secret");
+  const authHeader = req.headers.get("authorization");
   const url = new URL(req.url);
   const force = url.searchParams.get("force") === "1";
-  if (!force && secret !== process.env.CRON_SECRET as string) {
+
+if (!force && authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
