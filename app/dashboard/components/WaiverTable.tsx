0782ac17bce94d85bf4f0f2a3def51333070962b
DustinHeaps <60202144+DustinHeaps@users.noreply.github.com>
2025-06-02 11:45:31 -0400

    minor fix

--- app/dashboard/components/WaiverTable.tsx
+++ app/dashboard/components/WaiverTable.tsx
@@ -120,7 +120,7 @@ export default function WaiverTable({
                         />
                       </td>
                       <td className='px-4 py-3 text-sm whitespace-nowrap '>
-                        {waiver.signature?.name}
+                        {waiver.name}
                       </td>
                       <td className='px-4 py-3 text-sm whitespace-nowrap'>
                         {waiver.date
