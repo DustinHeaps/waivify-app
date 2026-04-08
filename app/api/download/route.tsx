77ff04a1cbf02a75033ef15989a2e2a1a33468e9
DustinHeaps <60202144+DustinHeaps@users.noreply.github.com>
2026-04-08 19:49:15 -0400

    updated Next Version

--- app/api/download/route.tsx
+++ app/api/download/route.tsx
@@ -45,7 +45,7 @@ export async function GET(req: Request) {
     />
   );
 
-  return new NextResponse(pdfBuffer, {
+  return new NextResponse(new Uint8Array(pdfBuffer), {
     headers: {
       "Content-Type": "application/pdf",
       "Content-Disposition": `attachment; filename=waiver-${waiver.signature.id}.pdf`,
